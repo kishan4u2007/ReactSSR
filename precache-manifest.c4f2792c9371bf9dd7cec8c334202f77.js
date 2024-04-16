c3b3eaad73372d0bddf7b308951a693f1dfa8280
kishan <kishan4u2007@gmail.com>
2024-04-16 18:12:00 +0530

    Updates

---
+++ precache-manifest.c4f2792c9371bf9dd7cec8c334202f77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06a67d32df282466a78a330f79a4eb1",
+    "url": "/ReactSSR/index.html"
+  },
+  {
+    "revision": "1b1c3a342fa941e4590d",
+    "url": "/ReactSSR/static/css/main.098c68da.chunk.css"
+  },
+  {
+    "revision": "c1451cc4b17b21c18e62",
+    "url": "/ReactSSR/static/js/2.8d289d76.chunk.js"
+  },
+  {
+    "revision": "1b1c3a342fa941e4590d",
+    "url": "/ReactSSR/static/js/main.f9cc5eb7.chunk.js"
+  },
+  {
+    "revision": "e58d971031df4f7507fb",
+    "url": "/ReactSSR/static/js/runtime-main.c72a643e.js"
+  }
+]);
