50b31e89a8c7cd15e687d76a9475fa784aa6b773
kishan <kishan4u2007@gmail.com>
2024-04-16 18:01:52 +0530

    adding build folder

---
+++ build/precache-manifest.4a518d86e8a0e7be05815258f2b8cf64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a58ba0305391c44244472a8ac55b21b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d147ce0c096b3e5c5b52",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1262a7331871cad8818d",
+    "url": "/static/js/2.189d1538.chunk.js"
+  },
+  {
+    "revision": "d147ce0c096b3e5c5b52",
+    "url": "/static/js/main.5df951e3.chunk.js"
+  },
+  {
+    "revision": "6dbe56ce70542469ff0d",
+    "url": "/static/js/runtime-main.04784543.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
